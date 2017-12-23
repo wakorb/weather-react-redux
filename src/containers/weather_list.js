a6aba2ed948dd4724b5878152a57fa610fcbe8be
Joshua Brokaw <joshuabrokaw@gmail.com>
2017-12-23 07:56:57 -0600

    final commit of weather map project

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index'
-import { Sparklines } from 'react-sparklines';
-import { SparklinesLine } from 'react-sparklines';
+
+import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component {
 
@@ -12,25 +13,14 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         const pressures = cityData.list.map(weather => weather.main.pressure);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>{name}</td>
-                <td>
-                    <Sparklines data={temps} width={180} height={120}>
-                        <SparklinesLine color='blue'/>
-                    </Sparklines>
-                </td>
-                <td>
-                    <Sparklines data={humidities} width={180} height={120}>
-                        <SparklinesLine color='green'/>
-                    </Sparklines>
-                </td>
-                <td>
-                    <Sparklines data={pressures} width={180} height={120}>
-                        <SparklinesLine color='red'/>
-                    </Sparklines>
-                </td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
+                <td><Chart data={temps} color='green' units='(K)'/></td>
+                <td><Chart data={pressures} color='orange' units='(hPa)'/></td>
+                <td><Chart data={humidities} color='blue' units='(%)'/></td>
             </tr>
         );
     }
@@ -41,9 +31,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
